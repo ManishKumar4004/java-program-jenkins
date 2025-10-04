9ffb146c4505885693c2c7baf3f20a9ca66f5f64
ManishKumar4004 <mt256136@gmail.com>
2025-10-05 01:58:01 +0530

    change

--- next.java
+++ next.java
@@ -3,5 +3,6 @@ public class next{
         System.out.println("This is the next Java program.");    
         System.out.println("It serves as a follow-up to the main program.");   
         System.out.println("Goodbye from the next program!");
+        System.out.println("Keep changes coming.");
     }
 }
