78d2684074b9daa294b23403462eb4f035fd3d50
ManishKumar4004 <mt256136@gmail.com>
2025-10-05 01:32:37 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class main{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");    
+        System.out.println("This is a sample Java program.");   
+        System.out.println(new Date()); 
+        System.out.println("Goodbye!");
+        System.out.println("This line has a typo.");
+    }
+}
