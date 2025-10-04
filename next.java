ae03eba062f6458e3c615c6a9c9b948346c05131
ManishKumar4004 <mt256136@gmail.com>
2025-10-05 01:54:35 +0530

    next

---
+++ next.java
@@ -0,0 +1,7 @@
+public class next{
+    public static void main(String[] args) {
+        System.out.println("This is the next Java program.");    
+        System.out.println("It serves as a follow-up to the main program.");   
+        System.out.println("Goodbye from the next program!");
+    }
+}
